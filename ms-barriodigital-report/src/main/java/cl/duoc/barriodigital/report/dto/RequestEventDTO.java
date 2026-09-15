0150225009c5d83834fbeb615ca71fe46a996f44
FelixO123 <fe.omenaca@duocuc.cl>
2026-09-15 02:55:58 -0300

    funcional_avance

--- ms-barriodigital-report/src/main/java/cl/duoc/barriodigital/report/dto/RequestEventDTO.java
+++ ms-barriodigital-report/src/main/java/cl/duoc/barriodigital/report/dto/RequestEventDTO.java
@@ -1,23 +1,25 @@
 package cl.duoc.barriodigital.report.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestEventDTO {
 
     private Long requestId;
     private Long procedureId;
     private String oldStatus;
     private String newStatus;
-    private LocalDateTime timestamp;
+    private Instant timestamp;
 }
